eeadb4a971962a1c1816732bbb79febe7855cbd7
Oycctem <ousstemm@gmail.com>
2024-03-24 15:12:56 -0700

    task 3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Flask web application """
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def index(text="is cool"):
+    """ Returns Python followed by the value of text"""
+    return 'Python {}'.format(text.replace('_', ' '))
+
+if __name__ == "__main__":
+    host = '0.0.0.0'
+    port = 5000
+    app.run(host=host, port=port)
