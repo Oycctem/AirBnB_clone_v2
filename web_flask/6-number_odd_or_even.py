6da8478d960fa22a37471096cc5f192001a6a379
Oycctem <ousstemm@gmail.com>
2024-03-25 01:51:31 +0000

    task 6

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -5,7 +5,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/number_odd_or_even/<n>', strict_slashes=False)
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def imanumber2(n):
     """ Returns if an int is odd or even """
     return render_template('6-number_odd_or_even.html', n=n)
